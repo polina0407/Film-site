968464779207d5a8822f3c8b1af6f1035549ecbf
polina0407 <147421205+polina0407@users.noreply.github.com>
2026-05-04 19:32:38 +0300

    Add files via upload

---
+++ Film-site-main/Film-site-main/film_site_project/film_site_app/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+
+    path("films/", FilmListView.as_view(), name="film_list"),
+    path("films/<int:id>/", FilmDetailView.as_view(), name="film_detail"),
+
+    path("actors/", ActorListView.as_view(), name="actor_list"),
+    path("actors/<int:id>/", ActorDetailView.as_view(), name="actor_detail"),
+
+    path("profile/", ProfileView.as_view(), name="profile"),
+
+    path("films/<int:id>/like/", LikeFilmView.as_view(), name="like_film"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
+]
