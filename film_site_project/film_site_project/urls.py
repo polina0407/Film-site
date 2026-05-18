3036b60bf4fc479abc68dde06b3fe072b28702a6
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-18 23:44:22 +0300

    Update urls.py

--- film_site_project/film_site_project/urls.py
+++ film_site_project/film_site_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve # Імпортуємо вбудований сервіс роздачі
