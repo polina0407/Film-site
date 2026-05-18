a05cf88d7f78889368bcce91753d2489cb243eb5
olha-tryhub-teacher <olha.tryhub.work@gmail.com>
2026-05-18 23:38:46 +0300

    Update urls.py

--- film_site_project/film_site_project/urls.py
+++ film_site_project/film_site_project/urls.py
@@ -20,8 +20,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve # Імпортуємо вбудований сервіс роздачі
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('film_site_app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# Напряму кажемо Django роздавати папку media за будь-яких умов
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
